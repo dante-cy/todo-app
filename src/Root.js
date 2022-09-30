1fbecaf40512712f2cbaf08452932c79d326975a
Dante <dante-mac@Dante-MacBookAir.local>
2022-09-30 09:10:51 +0900

    First commit

---
+++ src/Root.js
@@ -0,0 +1,17 @@
+import { HashRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import App from "./App";
+
+function Root() {
+  return (
+    <>
+      <RecoilRoot>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </RecoilRoot>
+    </>
+  );
+}
+
+export default Root;
